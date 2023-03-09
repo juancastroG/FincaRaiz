c76c8486b0ccbc38e8ea84189b0146c8143c6064
juancastroG <68920399+juancastroG@users.noreply.github.com>
2023-03-08 19:45:23 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,10 +1,31 @@
 from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 application = Flask(__name__)
 
 @application.route('/')
 def index():
-  return "holaaa shelsylinda"
+  # Set path Selenium
+  CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
+  s = Service(CHROMEDRIVER_PATH)
+  WINDOW_SIZE = "1920,1080"
+
+  # Options
+  chrome_options = Options()
+  chrome_options.add_argument("--headless")
+  chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+  chrome_options.add_argument('--no-sandbox')
+  driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+
+  # Get the response and print title
+  driver.get("https://www.fincaraiz.com.co/finca-raiz/venta/chapinero/bogota")
+  contenedor = driver.find_element_by_xpath('//*[@id="listingContainer"]')
+  return contenedor.text
+
+  driver.close()
   
 if __name__ == '__main__':
     application.run(debug=True)
